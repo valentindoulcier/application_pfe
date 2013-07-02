a6633bac070a49f513d8147215a486a40438b821
Simon Kesteloot <simon.kesteloot@etu.univ-tours.fr>
2013-07-02 14:56:31 +0200

    correction, transaction imbriqué

--- src/dao/AbstractDAO.java
+++ src/dao/AbstractDAO.java
@@ -100,11 +100,11 @@ public class AbstractDAO {
 	protected List<?> findAll(Session session, Class<?> clazz) {
 		List<?> objects = null;
 		try {
-			this.tx = session.beginTransaction();
+//			this.tx = session.beginTransaction();
 			
 			Query query = session.createQuery("from " + clazz.getName());
 			objects = query.list();
-			tx.commit();
+//			tx.commit();
 		} catch (HibernateException e) {
 			logger.fatal("TESSST");
 			handleException(e);
